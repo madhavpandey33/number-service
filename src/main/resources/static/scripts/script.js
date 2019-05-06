069f880e80cbbc3423f63e88b6e26e7481a35038
madhavpandey33 <madhavpandey33@gmail.com>
2019-05-05 22:16:52 -0600

    add error page and update ui

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,21 +1,35 @@
 $(function(){
-	hideErrorLabel();
+	hideErrors();
+	hideLodingGif();
 });
 
 function convert() {
 	var inputNumber = $("#inputNumber").val();
 	if(inputNumber) {
+		$("#animated-gif").show();
 		$.ajax({
 			url : "/v1/roman_number/"+inputNumber,
 			success : function(result) {
+				hideLodingGif();
 				$("#romanValue").val(result);
+			},
+			error: function(error){
+				hideLodingGif();
+				$("#serverError").html(error.responseJSON.message);
+				$("#errorResponse").show();
 			}
 		});
 	} else {
+		hideLodingGif();
 		$("#errorLabel").show();
 	}
 }
 
-function hideErrorLabel(){
+function hideErrors(){
 	$("#errorLabel").hide();
+	$("#errorResponse").hide();
+}
+
+function hideLodingGif(){
+	$("#animated-gif").hide();
 }
